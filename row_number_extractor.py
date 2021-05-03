4e04b613ae5d33c69dd9889c5ef3773fe13222ba
ariannamorettj <56439207+ariannamorettj@users.noreply.github.com>
2021-05-03 23:24:30 +0200

    Update row_number_extractor.py
    Comment to the code

--- row_number_extractor.py
+++ row_number_extractor.py
@@ -3,7 +3,14 @@ import csv
 import os
 
 """
-This function is aimed at returning an integer number, which is used as an index, in order to slice the input CSV file and process it only from the first still-non processed line on. The additional function of this part of code is that of creating and appending a dictionary for each publisher in publisher_data dictionary. In order to perform this set of actions, first of all a variable named num is initialized with the value 0, and then the existence in the directory of a file named “publisher_data.csv” is checked. In the case this file doesn’t exist, the variable num, whose value is 0, is returned. Since the output of this function is used as an index to individuate the last processed line of the CSV input file in the main function, in the case “publisher_data.csv” has not been created yet, no line of the original CSV has been processed, so the line to be processed is the first one. In the opposite case in which publisher_data.csv already exists, this file is read as a DictReader object, and for each element it contains, a key-value pair representing the publisher is added to publisher_data, whose key is the string of the name of the publisher, while the value is a dictionary with the following keys: “name”, “responsible_for_v”, “responsible_for_i”, “receiving_v”, “receiving_i”, and the associated values retrieved from the “publisher_data.csv” file. After the creation of this latter dictionary, the value of the variable num is updated by summing the value of “responsible_for_v” and the value of “responsible_for_i”. The sense of this passage is that of keeping track of the lines already processed in the original CSV file, and assuming that any citational data processed results in an increment by one either of the value of “responsible_for_v” or “responsible_for_i” in one publishers’ row in publisher_data.csv (read as a DictReader object),  by summing the values of 'responsible_for_i' and 'responsible_for_v' for each element of the DictReader object in a for loop it is possible to obtain the total number of citational data already processed. The final value of num at the end of the for loop is returned and used as start_index value in the main function.
+extract_row_number(publisher_data) returns an integer number and the dictionary 
+prefix_to_name_dict, containing a mapping between the prefixes and the names of 
+the respective publishers. The integer retrieved is used as an index representing 
+the number of the already successfully processed input rows. Accordingly, it is used 
+to slice the input CSV file and process it only from the first unprocessed line on, 
+in case the code run is interrupted. The additional function of this part of code is 
+that of creating a dictionary for each publisher in publisher_data dictionary, 
+exploiting the data provided in “publisher_data.csv”. 
 """
 
 
