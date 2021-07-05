5d070e0339489628b8006e17367dda4d8c1a76b0
arimoretti <arianna.moretti2@studio.unibo.it>
2021-07-05 09:04:33 +0200

    Update comment of invalid_dois.py
    Update comment of invalid_dois.py to make it explanatory of the current state of the code

--- invalid_dois.py
+++ invalid_dois.py
@@ -13,7 +13,7 @@ The input csv file is opened and the following data structures are created:
 1) publisher_data dictionary, aimed at storing dictionaries representing each publisher encountered;
 2) correct_dois_data list, aimed at storing citational data validated throughout the process;
 3) incorrect_dois_data list, aimed at storing citational data which could not be validated.
-Then, two variables (i.e. : start_index and prefix_to_member_code_dict) are assigned the values 
+Then, three variables (i.e. : start_index and prefix_to_member_code_dict, external_data_dict) are assigned the values 
 returned by the function extract_row_number(publisher_data), aimed at keeping track of 
 the lines of input material already processed. A recovery mechanism based on the creation 
 of ancillary files provides the possibility to overcome potential interruptions of the 
@@ -23,19 +23,20 @@ Accordingly, the software can work on a sliced amount of the input material.
 The input file is read with a csv reader and iterated row by row. 
 In this for loop, for each row processed, the validity of the receiving Digital Object 
 Identifier is checked through an API request. In the case the request is successful, 
-the citational data is appended to the list of validated citations (i.e.: correct_dois_data), 
+the citational data with its validation time is appended to the list of validated citations (i.e.: correct_dois_data), 
 and the publisher identification for both citing and cited doi proceeds with the execution 
-of extract_publishers_valid(row, publisher_data, prefix_to_member_code_dict). 
-Otherwise, the citational data is appended to the incorrect_dois_data list and the 
+of extract_publishers_valid(row, publisher_data, prefix_to_member_code_dict, external_data_dict). 
+Otherwise, the citational data is appended to the incorrect_dois_data list with its missed validation time, and the 
 publishers’ identification is managed with the correspective function for invalid 
-citational data, i.e.: extract_publishers_invalid(row, publisher_data, prefix_to_member_code_dict). 
+citational data, i.e.: extract_publishers_invalid(row, publisher_data, prefix_to_member_code_dict, external_data_dict). 
 In case of a connection error, an exception is raised. 
-Every hundred lines processed, the function write_to_csv(publisher_data, prefix_to_member_code_dict, 
-correct_dois_data, incorrect_dois_data) is executed and the lists correct_dois_data and 
-incorrect_dois_data are emptied, so as to save the processed material in files conceived for this purpose.
+Every n lines processed (where n is the number specified by the user as an input parameter) the function 
+write_to_csv(publisher_data, prefix_to_member_code_dict, external_data_dict, correct_dois_data, incorrect_dois_data) 
+is executed and the lists correct_dois_data and incorrect_dois_data are emptied, so as to save the processed material 
+in the files conceived for this purpose.
 Once all the lines of the original input file have been processed, the write_to_csv(publisher_data, 
-prefix_to_member_code_dict, correct_dois_data, incorrect_dois_data) function for cache files compilation 
-is called for the last time, and then the function to create the output file is executed. 
+prefix_to_member_code_dict, external_data_dict, correct_dois_data, incorrect_dois_data) function for cache files 
+compilation is called for the last time, and then the function to create the output file is executed. 
 """
 
 
